7b6e648ccfa96fcc234d61e911a27292cbce824f
RifqyYR <yudhiestrarifqy@gmail.com>
2024-04-11 11:43:27 +0800

    feat(plant): initialize plant and user model

---
+++ app/Models/Plant.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Ramsey\Uuid\Uuid;
+
+class Plant extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->{$model->getKeyName()} = (string) Uuid::uuid4();
+        });
+    }
+}
--- database/migrations/2024_04_11_030858_create_plants_table.php
+++ database/migrations/2024_04_11_030858_create_plants_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('plants', function (Blueprint $table) {
             $table->id();
             $table->uuid()->unique();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->date('date_input');
+            $table->time('time_input');
+            $table->integer('height')->nullable();
+            $table->string('filename');
+            $table->enum('plant_type', ['spinach', 'onion'])->default('spinach');
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('plants');
     }
 };
